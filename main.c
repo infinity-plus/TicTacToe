47a1dc090b2d3365a732c27ccb1a1128aa18a252
Harsh Navdhare <navdhareharsh2001@gmail.com>
2022-06-08 13:44:33 +0530

    Change formatting a bit

--- main.c
+++ main.c
@@ -2,19 +2,15 @@
 #include <stdlib.h>
 
 
-void showBoard(char board[3][3])
-{
-    for (int i = 0; i < 3; ++i)
-    {
+void showBoard(char board[3][3]) {
+    for (int i = 0; i < 3; ++i) {
         printf("\t\t\t  %c | %c  | %c  \n", board[i][0], board[i][1], board[i][2]);
         printf("\t\t\t----+----+----\n");
     }
 }
 
-int checkWin(char board[3][3], char sign)
-{
-    for (int i = 0; i < 3; i++)
-    {
+int checkWin(char board[3][3], char sign) {
+    for (int i = 0; i < 3; i++) {
         if (board[i][0] == sign && board[i][1] == sign && board[i][2] == sign)
             return 1;
         if (board[0][i] == sign && board[1][i] == sign && board[2][i] == sign)
@@ -26,8 +22,7 @@ int checkWin(char board[3][3], char sign)
     }
     return 0;
 }
-int main()
-{
+int main() {
     char ticTacToe[3][3];
     char entry = 'X';
     int pos, turn = 0;
@@ -41,18 +36,15 @@ int main()
             ticTacToe[i][j] = ' ';
     printf(". : : A simple two player Tic Tac Toe Game : : .\n");
     showBoard(ticTacToe);
-    while (1)
-    {
+    while (1) {
         entry = (turn % 2 == 0) ? 'O' : 'X';
         printf("Enter the position to play your turn (%c): ", entry);
         scanf("%d", &pos);
-        switch (pos)
-        {
+        switch (pos) {
         case 1:
             if (ticTacToe[0][0] == ' ')
                 ticTacToe[0][0] = entry;
-            else
-            {
+            else {
                 printf("Location already taken, try again!\n");
                 continue;
             }
@@ -62,8 +54,7 @@ int main()
         case 2:
             if (ticTacToe[0][1] == ' ')
                 ticTacToe[0][1] = entry;
-            else
-            {
+            else {
                 printf("Location already taken, try again!\n");
                 continue;
             }
@@ -72,8 +63,7 @@ int main()
         case 3:
             if (ticTacToe[0][2] = ' ')
                 ticTacToe[0][2] = entry;
-            else
-            {
+            else {
                 printf("Location already taken, try again!\n");
                 continue;
             }
@@ -82,8 +72,7 @@ int main()
         case 4:
             if (ticTacToe[1][0] == ' ')
                 ticTacToe[1][0] = entry;
-            else
-            {
+            else {
                 printf("Location already taken, try again!\n");
                 continue;
             }
@@ -92,8 +81,7 @@ int main()
         case 5:
             if (ticTacToe[1][1] == ' ')
                 ticTacToe[1][1] = entry;
-            else
-            {
+            else {
                 printf("Location already taken, try again!\n");
                 continue;
             }
@@ -102,8 +90,7 @@ int main()
         case 6:
             if (ticTacToe[1][2] == ' ')
                 ticTacToe[1][2] = entry;
-            else
-            {
+            else {
                 printf("Location already taken, try again!\n");
                 continue;
             }
@@ -112,8 +99,7 @@ int main()
         case 7:
             if (ticTacToe[2][0] == ' ')
                 ticTacToe[2][0] = entry;
-            else
-            {
+            else {
                 printf("Location already taken, try again!\n");
                 continue;
             }
@@ -122,8 +108,7 @@ int main()
         case 8:
             if (ticTacToe[2][1] == ' ')
                 ticTacToe[2][1] = entry;
-            else
-            {
+            else {
                 printf("Location already taken, try again!\n");
                 continue;
             }
@@ -132,8 +117,7 @@ int main()
         case 9:
             if (ticTacToe[2][2] == ' ')
                 ticTacToe[2][2] = entry;
-            else
-            {
+            else {
                 printf("Location already taken, try again!\n");
                 continue;
             }
@@ -144,8 +128,7 @@ int main()
             continue;
         }
         system("cls");
-        if (checkWin(ticTacToe, entry))
-        {
+        if (checkWin(ticTacToe, entry)) {
             showBoard(ticTacToe);
             printf("\n%c is Winner!!!", entry);
             return 0;
